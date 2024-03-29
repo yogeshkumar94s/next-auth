c724fbe93067ffacd9da292b01c0430370166504
Yogesh Kumar <yksainimbd@gmail.com>
2024-03-29 20:57:02 +0530

    middleware setup

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -1,7 +1,10 @@
 'use server'
  
 import * as z from 'zod';
+import { signIn } from "@/auth";
 import { LoginSchema } from '@/schemas';
+import { DEFAULT_LOGIN_REDIRECT } from '@/route';
+import { AuthError } from 'next-auth';
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
     const validatedFields = LoginSchema.safeParse(values);
@@ -10,8 +13,27 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
         return {error: "Invalid fields!" };
     };
 
-    return {
-        success: "Email sent successfully!"
-    };
+   const { email, password } = validatedFields.data;
 
+   try {
+    await signIn("credentials", {
+        email,
+        password,
+        redirectTo: DEFAULT_LOGIN_REDIRECT
+    })
+    
+   }catch (error) {
+    if (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case "CredentialsSignin": 
+                    return { error: "invalid credentials!" }
+                default: 
+                return { error: "Something went wrong!" }
+            }
+        }
+
+        throw error;
+    }
+   }
 };
--- src/actions/register.ts
+++ src/actions/register.ts
@@ -1,7 +1,7 @@
 'use server'
  
 import { db } from '@/lib/db';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import * as z from 'zod';
 
 import { RegisterSchema } from '@/schemas';
